96556e094d93155cb8811ca79dbbc48125e09d88
Jirka Marsik <jiri.marsik89@gmail.com>
2016-08-04 01:36:23 +0200

    Making TARDIS work with newer CIDER

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.cemerick/pomegranate "0.2.0"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [org.clojure/core.logic "0.8.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.cemerick/pomegranate "0.3.1"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/core.logic "0.8.10"]]
   :repl-options {:init (do (require 'tardis.core)
                            (tardis.core/wake-up-tardis))})
